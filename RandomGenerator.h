2a8d74277f94225a6516ddd9533c1a2e888f82e9
Ульяна Дудорова ТКИ-242 <uliana.dudorova@gmail.com>
2026-09-22 20:55:57 +0300

    Update RandomGenerator.h

--- RandomGenerator.h
+++ RandomGenerator.h
@@ -1,26 +1,31 @@
 #pragma once
 
+#include "Generator.h"
+#include <random>
+
 namespace miit::algebra {
 
-    /**
-     * @brief Абстрактный генератор целочисленных значений
-     *
-     * Базовый класс для всех способов заполнения матрицы: случайными
-     * числами (RandomGenerator), значениями с клавиатуры/из потока
-     * (IStreamGenerator) и константой (ConstantGenerator). Конкретный
-     * алгоритм генерации реализуется в классе-наследнике.
+    /
+     * @brief Генератор случайных целых чисел в заданном диапазоне
      */
-    class Generator {
+    class RandomGenerator : public Generator {
+    private:
+        std::mt19937 generator;
+        std::uniform_int_distribution<int> distribution;
+
     public:
-        /**
-         * @brief Виртуальный деструктор
+        /
+         * @brief Конструктор
+         * @param min нижняя граница диапазона
+         * @param max верхняя граница диапазона
          */
-        virtual ~Generator() = default;
+        RandomGenerator(const int min, const int max);
 
         /**
-         * @brief Сгенерировать очередное значение
-         * @return сгенерированное значение
+         * @brief Сгенерировать случайное число из диапазона
+         * @return случайное значение
          */
-        
+        int generate() override;
+    };
 
 } // namespace miit::algebra
